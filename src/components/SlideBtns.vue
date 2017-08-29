a5c141685482264d5af2808ec39ff77ca9311b40
rulelee <1007718228@qq.com>
2017-08-29 23:00:34 +0800

    修改首页按钮靠右浮动

--- src/components/SlideBtns.vue
+++ src/components/SlideBtns.vue
@@ -1,32 +1,36 @@
 <template>
     <!--放左边是fixed-left   放右边是fixed-right-->
-    <div class="fixed-slide fixed-left" >
-        <div v-if="type=='back'">
+    <div>
+        <div class="fixed-slide fixed-left" v-if="type=='back'">
             <a href="javascript:;" class="slide-btn-wrap slide-btn-01" @click="onNavigation()">
-                <i></i><em>{{$lang('返回')}}</em>
+                <i></i>
+                <em>{{$lang('返回')}}</em>
             </a>
         </div>
-        <div v-if="type=='list'">
+        <div class="fixed-slide fixed-right" v-if="type=='list'">
             <a href="javascript:;" class="slide-btn-wrap slide-btn-02" @click="onNavigation('login')">
-                <i></i><em>{{$lang('联系我们')}}</em>
+                <i></i>
+                <em>{{$lang('联系我们')}}</em>
             </a>
             <a href="javascript:;" class="slide-btn-wrap slide-btn-03" @click="onNavigation('choise')">
-                <i></i><em>{{$lang('帮助中心')}}</em>
+                <i></i>
+                <em>{{$lang('帮助中心')}}</em>
             </a>
         </div>
     </div>
 </template>
+
 <script>
-    export default{
-        props:["to", "type"],
-        methods:{
-            onNavigation(name){
-                if(name){
-                    this.$router.push({name:name})
-                }else{
-                    this.$router.go(-1)
-                }
-            }  
+export default {
+    props: ["to", "type"],
+    methods: {
+        onNavigation(name) {
+            if (name) {
+                this.$router.push({ name: name })
+            } else {
+                this.$router.go(-1)
+            }
         }
     }
+}
 </script>
