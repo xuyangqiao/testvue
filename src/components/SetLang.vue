d85825816f02846da4fdd4cf2484279da28fb001
rulelee <1007718228@qq.com>
2017-11-07 00:49:42 +0800

    补全文件

---
+++ src/components/SetLang.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <p>
+            <a href="javascript:;" class="nh-a-03" :class="{selected:(lang=='CN' || lang==null)}" @click="$setLang('CN')">中文</a>
+            <span>|</span>
+            <a href="javascript:;" class="nh-a-03" :class="{selected:lang=='EN'}" @click="$setLang('EN')">English</a>
+        </p>
+    </div>
+</template>
